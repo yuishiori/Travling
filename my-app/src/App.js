c36b55cb2aca75058e4dc8c844f6a63f79d664cf
yuishiori <yuishiori@gmail.com>
2025-06-22 10:42:53 +0800

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="app-header">
+        <h1 className="app-title">
+          Travling
+        </h1>
       </header>
+      <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+        <button>按鈕</button>
+      </a>
     </div>
   );
 }
